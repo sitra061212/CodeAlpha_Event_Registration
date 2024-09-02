14ae92973e43a1aa24c0db4a9588e1fdbbb3ef4c
sitra <sitra.f.khairo515@gmail.com>
2024-09-02 10:21:48 +0000

    database config

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import { connect } from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await connect('mongodb://birna123:admin123@codealpha-shard-00-00.3b0ww.mongodb.net:27017,codealpha-shard-00-01.3b0ww.mongodb.net:27017,codealpha-shard-00-02.3b0ww.mongodb.net:27017/EventRegistration?replicaSet=atlas-bi2kdu-shard-0&ssl=true&authSource=admin');
+    await connect(process.env.MONGO_URI);
     console.log('MongoDB connected...');
   } catch (err) {
     console.error(err.message);
