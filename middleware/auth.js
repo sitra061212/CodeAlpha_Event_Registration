82cf5e182d741166f97c0a63027a3759e0eeec87
sitra <sitra.f.khairo515@gmail.com>
2024-09-02 10:21:58 +0000

    middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,12 +4,12 @@ const { verify } = pkg;
 export default function (req, res, next) {
   // Extract token from Authorization header
   const authHeader = req.header('Authorization');
-  const token = authHeader && authHeader.split(' ')[1]; // Extract token by removing "Bearer" prefix
+  const token = authHeader && authHeader.split(' ')[1]; 
 
   if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
 
   try {
-    const decoded = verify(token, 'your_jwt_secret');
+    const decoded = verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
     next();
   } catch (err) {
